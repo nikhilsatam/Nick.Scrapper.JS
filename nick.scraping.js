ad1490a97a12cbc918fa5bbeaa735e0856c0ded1
nikhilsatam <nikhilsatam27@gmail.com>
2014-03-27 17:06:13 +0530

    Create nick.scraping.js
    Main JS Module

---
+++ nick.scraping.js
@@ -0,0 +1,17 @@
+var $isJQUERY;
+var $isInitialized;
+var $contentHTML;
+var $Nick = new function () {
+    this.Scrapper = new function () {
+
+        this.$proxyCall = function (URL, method, params, callback) {
+            if ($isInitialized) {
+            }
+        }
+    }
+    this.MsgBox = function (c) {
+        alert(c);
+    }
+};
+
+
